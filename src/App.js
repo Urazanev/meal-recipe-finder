3f72e45836bbe90b7f01081d908d78610e22b823
marsel.urazanev <marsel.urazanev@ext.csobpoj.cz>
2022-03-22 08:41:17 +0100

    lazy loading added for singleMealPage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import './App.css'
-import React from 'react'
+import React, {lazy, Suspense} from 'react'
 import HomePage from './components/HomePage'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
-import SingleMealPage from './components/SingleMealPage'
+
+const SingleMealPage = lazy(() => import( './components/SingleMealPage'));
 
 function App () {
   return (
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/meal/:mealId" element={<SingleMealPage />} />
-        </Routes>
-      </Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/meal/:mealId" element={<SingleMealPage />} />
+          </Routes>
+        </Router>
+      </Suspense>
+
   )
 }
 
